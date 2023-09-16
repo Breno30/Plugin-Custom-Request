df4a5259839a4695f9a146d8df7df7727ba75bcf
Breno30 <brenodonascimentosilvaa@gmail.com>
2023-09-16 07:55:34 -0300

    create custom post

--- custom-request.php
+++ custom-request.php
@@ -5,3 +5,42 @@ Description: Create a custom post type for Requests.
 Version: 1.0
 Author: Your Name
 */
+
+function create_requests_post_type()
+{
+    $labels = [
+        'name' => 'Requests',
+        'singular_name' => 'Request',
+        'add_new' => 'Add New',
+        'add_new_item' => 'Add New Request',
+        'edit_item' => 'Edit Request',
+        'new_item' => 'New Request',
+        'view_item' => 'View Request',
+        'view_items' => 'View Requests',
+        'search_items' => 'Search Requests',
+        'not_found' => 'No Requests found',
+        'not_found_in_trash' => 'No Requests found in Trash',
+        'parent_item_colon' => 'Parent Request:',
+        'menu_name' => 'Requests',
+    ];
+
+    $args = [
+        'labels' => $labels,
+        'public' => true,
+        'has_archive' => true,
+        'publicly_queryable' => true,
+        'query_var' => true,
+        'rewrite' => ['slug' => 'requests'],
+        'capability_type' => 'post',
+        'hierarchical' => false,
+        'menu_icon' => 'dashicons-admin-site-alt3',
+        'supports' => [
+            'title',
+            'custom-fields',
+        ],
+    ];
+
+    register_post_type('requests', $args);
+}
+
+add_action('init', 'create_requests_post_type');
